b10b48f2fd74c72450b30a75907cf008695f701e
Sérgio Rafael Siqueira <sergio@inbep.com.br>
2018-10-16 15:19:08 -0300

    Add doctest

--- sketchengine.py
+++ sketchengine.py
@@ -5,9 +5,19 @@ from pyf import map
 
 
 def concordance(word: str):
+    """
+    >>> from sketchengine import concordance
+    >>> concordance('feet')[0]
+    'The "widow boats" averaged 15 feet.'
+    """
     url = 'https://skell.sketchengine.co.uk/run.cgi/concordance?lpos=&query={}'
     browser = StatefulBrowser()
     browser.open(url.format(word))
     tbody = browser.get_current_page().select('#conc_content tbody tr')
 
     return tbody | map(lambda x: ''.join(x.findAll('td')[1].text.strip().splitlines()))
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod(verbose=True)
